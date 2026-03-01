f93a9332ecc8be22a06434c31edb5710e29b1d89
Roberto Mejia Lacayo <63435327+RobertoMejiaLacayo@users.noreply.github.com>
2026-02-28 22:08:19 -0800

    Create loans_cleaned table with outcome categories
    Create a cleaned version of the loans table with categorized outcomes and payment status.

---
+++ sql/03_data_cleaning.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE TABLE `lending_club_raw.loans_cleaned` AS
+
+SELECT
+annual_inc,
+dti,
+emp_length,
+funded_amnt,
+grade,
+home_ownership,
+issue_date,
+int_rate_pct,
+loan_amnt,
+loan_status, -- keeping the original for ease of reference
+CASE
+  WHEN loan_status LIKE '%Fully Paid%' THEN 'Fully Paid'
+  WHEN loan_status LIKE '%Charged Off%' THEN 'Charged Off'
+  WHEN loan_status LIKE '%Default%' THEN 'Charged Off'
+  WHEN loan_status LIKE '%Current%' THEN 'Active'
+  WHEN loan_status LIKE '%Late%' THEN 'Active'
+  WHEN loan_status = 'In Grace Period' THEN 'Active'
+  ELSE 'Other'
+END AS outcome_category,
+CASE
+ WHEN loan_status LIKE '%Fully Paid%' THEN 1
+ ELSE 0
+END AS is_paid,
+purpose,
+recoveries,
+sub_grade,
+term_months,
+total_pymnt
+FROM `lending_club_raw.loans_clean`;
